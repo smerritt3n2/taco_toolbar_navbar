f44bae7da46e0968e79e4fafb2998fa07ac47989
Samuel Merritt <65547748+smerritt3n2@users.noreply.github.com>
2021-03-03 15:50:41 +0000

    Image update

---
+++ taco-toolbar (coding challenge)/js/scripts.js
@@ -0,0 +1,9 @@
+/* When user clicks on "Icon", display submenu */
+function showNav() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
